b571b6e77912336c53ef0126289baadb92b34ed6
subhashraaj <150801940+subhashraaj@users.noreply.github.com>
2023-12-14 09:23:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
+function result(val1,val2){
+	console.log((val1*2)+val2);	
+}
+
 let value1=prompt("Enter the first value");
 let value2=prompt("Enter the second value");
-let res=(value1*2)+value2;
-Alert("The final result is:"+res);
+Alert(result(value1,value2));
